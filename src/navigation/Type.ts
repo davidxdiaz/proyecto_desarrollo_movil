366b8b8fdc6ab65c85ac83c5493f6f1f8eb2818a
heydI1-glitch <heydimolina019@gmail.com>
2026-08-30 15:15:28 -0600

    definición de tipos y parámetros tipados para rutas de navegación

---
+++ src/navigation/Type.ts
@@ -0,0 +1,8 @@
+// Definimos un tipo (Type) que describe las rutas disponibles en nuestro Tab Navigator
+// y los parámetros que cada pantalla puede recibir. En este caso, ninguna recibe parámetros iniciales (undefined).
+export type RootTabParamList = {
+  Home: undefined;
+  Perfiles: undefined;
+  Documentos: undefined;
+  Ajustes: undefined;
+};
